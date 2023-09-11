07943581d9db0d9af148567f9f7abc0bf645d835
Christopher S <christophersetiabudi@gmail.com>
2023-09-11 17:50:04 -0400

    Creating preprocessing function

--- PCT.py
+++ PCT.py
@@ -50,7 +50,16 @@ def readVideo(path: str, channels: int = 3):
     return matrix
 
 
-def PCT(video: np.ndarray, name: str, method: str = 'PCA'):
+def normalize(x):
+    '''
+    Normalize a 2D numpy array to 0-255
+    '''
+    min = np.min(x)
+    max = np.max(x)
+    return ((x - min) / (max - min) * 255).astype('uint8')
+
+
+def PCT(video: np.ndarray, path: str, method: str = 'PCA'):
     '''
     Performs Principal Component Thermography on a video with 1 channel
 
@@ -58,8 +67,8 @@ def PCT(video: np.ndarray, name: str, method: str = 'PCA'):
     ----------
     video : numpy array
         Numpy array of the video file
-    name : str
-        Name of file to save results as
+    path : str
+        Path to save results in (without extension)
     method : str
         Method to use for PCT. Can be 'PCA' or 'SVD'
     '''
@@ -81,11 +90,6 @@ def PCT(video: np.ndarray, name: str, method: str = 'PCA'):
         EOF2 = principalComponents[:, 1].reshape(height, width)
 
         # Normalize EOFs
-        def normalize(x):
-            min = np.min(x)
-            max = np.max(x)
-            return ((x - min) / (max - min) * 255).astype('uint8')
-        
         print('Normalizing results...')
         EOF1 = normalize(EOF1)
         EOF2 = normalize(EOF2)
@@ -99,8 +103,8 @@ def PCT(video: np.ndarray, name: str, method: str = 'PCA'):
         cv2.imshow("EOF2", EOF2)
 
         # Save EOFs as images
-        cv2.imwrite(name + '_EOF1.png', EOF1)
-        cv2.imwrite(name + '_EOF2.png', EOF2)
+        cv2.imwrite(path + '_EOF1.png', EOF1)
+        cv2.imwrite(path + '_EOF2.png', EOF2)
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
@@ -122,15 +126,175 @@ def PCT(video: np.ndarray, name: str, method: str = 'PCA'):
         # Show EOFs as thermograms
         
         fig1 = px.imshow(EOF1, title='EOF1')
-        fig1.write_image(name + '_EOF1.png', format="png")
+        fig1.write_image(path + '_EOF1.png', format="png")
 
         fig2 = px.imshow(EOF2, title='EOF2')
-        fig2.write_image(name + '_EOF2.png')
+        fig2.write_image(path + '_EOF2.png')
+
+
+def createMask(vid1Name, vid2Name, debug: bool = False):
+    '''
+    Creates a mask for the videos by selecting a region of interest (ROI), cropping
+     the video to that region, and saving the cropped video.
+
+    Assumes that the videos have the same dimensions, the ROI is the same for 
+    both videos, and they have an .mp4 extension.
+
+    Parameters
+    ----------
+    vid1Name : str
+        Path to the first video (without extension)
+    vid2Name : str
+        Path to the second video (without extension)
+
+    Returns
+    -------
+    output1Name : str
+        Path to the first cropped video (with extension)
+    output2Name : str
+        Path to the second cropped video (with extension)
+    '''
+
+    # Define variables
+    output1Name = vid1Name + '_mask.mp4'
+    output2Name = vid2Name + '_mask.mp4'
+
+    cap1 = cv2.VideoCapture(vid1Name + '.mp4')
+    cap2 = cv2.VideoCapture(vid2Name + '.mp4')
+    fps = 30
+
+    # Read the first frame to determine frame dimensions
+    ret, frame = cap1.read()
+    if not ret:
+        print("Error: Cannot read video file.")
+        exit()
+
+    # Create a window to display the video and select ROI
+    cv2.namedWindow('Select ROI')
+    x, y, w, h = cv2.selectROI('Select ROI', frame)
+
+    # Create the video writer
+    writer1 = cv2.VideoWriter(output1Name, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+    writer2 = cv2.VideoWriter(output2Name, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+
+    # Get the ROI for each frame
+    while True:
+        ret, frame = cap1.read()
+        if not ret:
+            break
+
+        # Crop the frame to the selected ROI
+        roiFrame = frame[y:y+h, x:x+w]
+        
+        # Display the ROI frame
+        if debug:
+            cv2.imshow('ROI Frame 1', roiFrame)
+
+        # Write the frame to the video file
+        writer1.write(roiFrame)
+
+        # Press 'q' to exit the loop
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    
+    while True:
+        ret, frame = cap2.read()
+        if not ret:
+            break
+
+        # Crop the frame to the selected ROI
+        roiFrame = frame[y:y+h, x:x+w]
+        
+        # Display the ROI frame
+        if debug:
+            cv2.imshow('ROI Frame 2', roiFrame)
+
+        # Write the frame to the video file
+        writer2.write(roiFrame)
+
+        # Press 'q' to exit the loop
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+            
+    # Release resources
+    cap1.release()
+    cap2.release()
+    writer1.release()
+    writer2.release()
+    cv2.destroyAllWindows()
+
+    return output1Name, output2Name
+
+
+def preProcess(afterFile: str, beforeFile: str, debug: bool = False):
+    '''
+    Highlights defects by performing 'cold substraction' on the heated and unheated videos
+
+    Parameters
+    ----------
+    afterFile : str
+        Path to the after video (without extension)
+    beforeFile : str
+        Path to the before video (without extension)
+    '''
+
+    # Create Mask
+    print("Creating mask...")
+    afterMask, beforeMask = createMask(afterFile, beforeFile, debug)
+
+    # Read the videos
+    afterVid = readVideo(afterMask)
+    beforeVid = readVideo(beforeMask)
+
+    print("")
+    # Get the pixelwise mean across all frames
+    afterMean = np.mean(afterVid, axis=0)
+    beforeMean = np.mean(beforeVid, axis=0)
+
+    # Normalize the means
+    afterNormalized = normalize(afterMean)
+    beforeNormalized = normalize(beforeMean)
+
+    # Get the difference between the heated and unheated means
+    diff = afterNormalized - beforeNormalized
+
+    # Show the results
+
+    copyAfter = afterNormalized.copy()
+    copyBefore = beforeNormalized.copy()
+
+    cv2.applyColorMap(copyAfter, cv2.COLORMAP_JET)
+    cv2.applyColorMap(copyBefore, cv2.COLORMAP_JET)
+    cv2.applyColorMap(diff, cv2.COLORMAP_JET)
+
+    cv2.imshow("After (Mean + Normalized)", copyAfter)
+    cv2.imshow("Before (Mean + Normalized)", copyBefore)
+    cv2.imshow("Difference", diff) 
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    cv2.imwrite(afterFile + '_processed.png', copyAfter)
+    cv2.imwrite(beforeFile + '_processed.png', copyBefore)
+    cv2.imwrite(afterFile + '_diff.png', diff)
+    
+    # Perform PCT on the after video if wanted
+    doPCT = input("Perform PCT on the after video? (y/n): ")
+
+    if doPCT == 'y':
+        PCT(afterVid, afterFile, 'PCA')
 
 
 # Test code
 if __name__ == '__main__':
-    print("Reading Video...")
-    video = readVideo('vid-edited2.mp4')
+    # print("Reading Video...")
+    # video = readVideo('vid-edited2.mp4')
+    # before = readVideo('before-edited2.mp4')
 
-    PCT(video, 'SVD')
+    # PCT(video, 'Heated', 'PCA')
+    # PCT(before, 'Unheated', 'PCA')
+
+    # heated = cv2.imread('Heated_EOF1.png')
+    # unheated = cv2.imread('Unheated_EOF1.png')
+
+    preProcess('images/left_after_09_11', 'images/left_before_09_11', debug=False)
