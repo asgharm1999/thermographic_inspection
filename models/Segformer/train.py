d904e54e43d6f18820e3c3d8ca322b63d1249ee6
Christopher S <christophersetiabudi@gmail.com>
2023-09-15 14:00:50 -0400

    Finished Segformer pipeline
    Training pipeline for segformer finished but untested.
    
    Will test and update if any errors present

--- models/Segformer/train.py
+++ models/Segformer/train.py
@@ -10,47 +10,88 @@ from transformers import (
 )
 import evaluate
 
-from utils import Transform
+from utils import Transform, ComputeMetrics
 
-# Parse Arguments 
-parser = argparse.ArgumentParser(description='Train Segformer')
-parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate')
-parser.add_argument('--epochs', type=int, default=50, help='Number of epochs')
-parser.add_argument('--batchSize', type=int, default=6, help='Batch size')
-parser.add_argument('--earlyStop', type=int, default=10, help='Early stopping patience')
-parser.add_argument('--name', type=str, default='Test', help='Name of the run')
-parser.add_argument('--scheduler', type=str, default='polynomial', help='Learning rate scheduler')
-parser.add_argument('--modelName', type=str, default='nvidia/mit-b0', help='Model name')
+# Parse Arguments
+parser = argparse.ArgumentParser(description="Train Segformer")
+parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate")
+parser.add_argument("--epochs", type=int, default=50, help="Number of epochs")
+parser.add_argument("--batchSize", type=int, default=6, help="Batch size")
+parser.add_argument("--earlyStop", type=int, default=10, help="Early stopping patience")
+parser.add_argument("--name", type=str, default="Test", help="Name of the run")
+parser.add_argument("--scheduler", type=str, default="polynomial", help="Learning rate scheduler")
+parser.add_argument("--modelName", type=str, default="nvidia/mit-b0", help="Model name")
+parser.add_argument("--evalMetric", type=str, default="mean_iou", help="Evaluation metric")
 
 args = parser.parse_args()
 
 # Initialize WandB
 wandb.init(
-    project='projectName',  # TODO: Set project name
+    project="projectName",  # TODO: Set project name
     config={
-        'modelName': args.modelName,
-        'batchSize': args.batchSize,
-        'epochs': args.epochs,
-        'lr': args.lr,
-        'scheduler': args.scheduler,
-        'earlyStop': args.earlyStop,
-        'evalMetric': 'mean_iou'
+        "modelName": args.modelName,
+        "batchSize": args.batchSize,
+        "epochs": args.epochs,
+        "lr": args.lr,
+        "scheduler": args.scheduler,
+        "earlyStop": args.earlyStop,
+        "evalMetric": args.evalMetric,
     },
     name=args.name,
 )
 config = wandb.config
 
 # Load dataset
-train, test = load_dataset('nameOfDataset', split=['train', 'test'])  # TODO: Set dataset name
+train, test = load_dataset("nameOfDataset", split=["train", "test"])  # TODO: Set dataset name
 
-id2label = {0: 'background', 1: 'object'}  # TODO: Set id2label
+id2label = {0: "background", 1: "object"}  # TODO: Set id2label
 label2id = {v: k for k, v in id2label.items()}
 numLabels = len(id2label)
 
 # Preprocess dataset
 preprocessor = SegformerImageProcessor()
-train.set_transform(Transform(preprocessor, transform='None', isTrain=True))
-test.set_transform(Transform(preprocessor, transform='None', isTrain=False))
+train.set_transform(Transform(preprocessor, transform="None", isTrain=True))
+test.set_transform(Transform(preprocessor, transform="None", isTrain=False))
 
 # Define evaluation metrics
 metric = evaluate.load(config.evalMetric)
+metricComputer = ComputeMetrics(metric, id2label, numLabels)
+
+# Define model
+model = SegformerForSemanticSegmentation.from_pretrained(
+    config.modelName, num_labels=numLabels, id2label=id2label, label2id=label2id
+)
+wandb.watch(model)
+
+# Set up training arguments
+trainArgs = TrainingArguments(
+    output_dir="runs/" + args.name,
+    learning_rate=config.lr,
+    num_train_epochs=config.epochs,
+    per_device_train_batch_size=config.batchSize,
+    per_device_eval_batch_size=config.batchSize,
+    save_total_limit=1,
+    save_strategy="epoch",
+    evaluation_strategy="epoch",
+    load_best_model_at_end=True,
+    remove_unused_columns=False,
+    lr_scheduler_type=config.scheduler,
+    optim="adamw_torch",
+)
+
+# Set up callbacks
+earlyStop = EarlyStoppingCallback(early_stopping_patience=config.earlyStop)
+
+# Train model
+trainer = Trainer(
+    model=model,
+    args=trainArgs,
+    train_dataset=train,
+    eval_dataset=test,
+    compute_metrics=metricComputer.computeMetrics,
+    callbacks=[earlyStop],
+)
+trainer.train()
+
+# End
+wandb.finish()
--- models/Segformer/utils.py
+++ models/Segformer/utils.py
@@ -1,13 +1,16 @@
-'''
+"""
 utils.py
 
 Utility functions for Segformer training
-'''
+"""
 
 
 # Import Libraries
-import albumentations as A 
+import albumentations as A
 import numpy as np
+
+from torch import no_grad, from_numpy
+from torch.nn import functional as F
 
 # Transforms
 noTransform = A.Compose([A.Resize(512, 512)], is_check_shapes=False)
@@ -19,7 +22,7 @@ lightTransform = A.Compose(
 
 
 class Transform:
-    ''' 
+    """
     Creates a transform object that can be used to transform images and masks
 
     Parameters
@@ -35,31 +38,63 @@ class Transform:
     -------
     obj
         Transform object
-    '''
+    """
 
-    def __init__(self, preprocessor, transform='none', isTrain=True):
+    def __init__(self, preprocessor, transform: str = "none", isTrain: bool = True):
         self.preprocessor = preprocessor
         self.isTrain = isTrain
 
-        if transform == 'none':
+        if transform == "none":
             self.transform = noTransform
-        elif transform == 'light':
+        elif transform == "light":
             self.transform = lightTransform
         else:
-            raise ValueError('Invalid transform')
+            raise ValueError("Invalid transform")
 
     def __call__(self, batch):
         if self.isTrain:
             images, labels = [], []
 
-            for image, mask in zip(batch['image'], batch['mask']):
+            for image, mask in zip(batch["image"], batch["mask"]):
                 transformed = self.transform(image=np.array(image), mask=np.array(mask))
-                images.append(transformed['image'])
-                labels.append(transformed['mask'])
-            
+                images.append(transformed["image"])
+                labels.append(transformed["mask"])
+
             return self.preprocessor(images, labels)
-        
+
         else:
-            images = [x for x in batch['image']]
-            labels = [x for x in batch['mask']]
+            images = [x for x in batch["image"]]
+            labels = [x for x in batch["mask"]]
             return self.preprocessor(images, labels)
+
+
+# Metrics
+class ComputeMetrics:
+    def __init__(self, metric, id2label, numLabels):
+        self.metric = metric
+        self.id2label = id2label
+        self.numLabels = numLabels
+
+    def computeMetrics(self, pred):
+        with no_grad():
+            logits, labels = pred
+            logits = from_numpy(logits)
+            logits = F.interpolate(
+                logits, size=labels.shape[-2:], mode="bilinear", align_corners=False
+            ).argmax(dim=1)
+
+            predLabels = logits.detach().cpu().numpy()
+            metrics = self.metric.compute(
+                predictions=predLabels,
+                references=labels,
+                num_labels=self.numLabels,
+                ignore_index=-1,
+            )
+
+            perCatAcc = metrics.pop("per_category_accuracy").tolist()
+            perCatIOU = metrics.pop("per_category_iou").tolist()
+
+            metrics.update({f"accuracy_{self.id2label[i]}": v for i, v in enumerate(perCatAcc)})
+            metrics.update({f"iou_{self.id2label[i]}": v for i, v in enumerate(perCatIOU)})
+
+            return {"val_" + k: v for k, v in metrics.items()}
