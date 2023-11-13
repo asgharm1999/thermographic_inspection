41c875e69d0780ffe65f21cb683502edf3159c62
Christopher S <christophersetiabudi@gmail.com>
2023-11-13 14:34:01 -0500

    Set up basic backend server

--- server/server.py
+++ server/server.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 # Create the application instance
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/api/home", methods=["GET"])
 def home():
-    return jsonify({
-        "message": "Hello world!"
-    })
+    return jsonify({"message": "If you see this, the backend is working!"})
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Use port 8080 because default port 5000 doesn't work with CORS
+    app.run(debug=True, port=8080)
